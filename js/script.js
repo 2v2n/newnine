2ca40661c25948c6a4652290b58d9ba000d71e32
even <hyunwoo0212@naver.com>
2025-07-30 12:53:16 +0900

    firstcommit

---
+++ js/script.js
@@ -0,0 +1,37 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// setInterval(실행할함수 , 몇초에 한번씩)
+setInterval(function(){
+    pushSlides(1)
+}, 2000);
+
+const input = document.getElementById("searchInput");
+const form = document.getElementById("search");
+
+input.addEventListener("focus", function(){
+    form.classList.add('focus');
+});
+
+input.addEventListener("blur", function(){
+    form.classList.remove("focus");
+});
+
+function pushSlides(n){
+    showSlides(slideIndex += n);
+}
+function currentSlides(n){
+    showSlides(slideIndex -= n);
+}
+
+function showSlides(n){
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    if(n > slides.length) {
+        slideIndex = 1;
+    }
+    for(i = 0; i < slides.length; i ++) {
+        slides[i].style.display= "none";
+    }
+    slides[slideIndex-1].style.display = "block";
+}
